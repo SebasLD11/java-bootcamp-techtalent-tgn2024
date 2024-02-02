e858f09528d8597ff71f5310378717963b391130
Sebas Lopez <SEBASSRYO08@GMAIL.COM>
2024-02-02 11:02:21 +0100

    1er Commit.TareasGit.SL
    Tareas con github, sebas lópez

---
+++ GitHub.Tareas.SL/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitHub.Tareas.SL {
+}
---
+++ GitHub.Tareas.SL/src/proyecto1/SL/HolaMundo.java
@@ -0,0 +1,10 @@
+package proyecto1.SL;
+
+public class HolaMundo {
+	
+	public static void main (String[] args) {
+	
+		System.out.println ("Hola Mundo");
+	}
+
+}
